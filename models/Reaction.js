9cc8d5302c42c94aac9c97bb2d279bb77bc2c3e3
chentriangoes <chentriangoes@gmail.com>
2022-04-17 17:23:28 +1000

    8th commit_Reaction Model

---
+++ models/Reaction.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+// Schema to create Reaction model
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Types.ObjectId,
+        default: new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: [280]
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    }
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false,
+});
+
+module.exports = reactionSchema;
