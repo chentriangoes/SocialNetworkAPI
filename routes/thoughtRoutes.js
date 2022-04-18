06fbaa3b150409506f87d0247891b1174e559a2f
chentriangoes <chentriangoes@gmail.com>
2022-04-18 16:17:37 +1000

    11th commit_thoughtRoutes

---
+++ routes/thoughtRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+
+const {
+    getThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../controllers/thoughtController');
+
+router
+.route('/')
+.get(getThoughts)
+.post(createThought);
+
+router
+.route('/:id')
+.get(getThoughtById)
+.post(updateThought)
+.delete(deleteThought);
+
+router
+.route('/:thoughtId/reactions')
+.post(addReaction)
+.delete(deleteReaction);
+
+module.exports = router;
